75cdc6f95c8674156893c5dec4decf02c6c39698
Tigran Manasyan <manasyan_tigran@mail.ru>
2025-09-05 11:58:58 +0400

    [ADS-2588] Support deletes in Kafka connector (#3)
    * [ADS-2588] Support deletes in Kafka connector
    
    * [ADS-2673] Support multiple cdc operations for single Iceberg operation
    
    * [ADS-2674] Add an ignored source operations option to the Kafka connector
    
    ---------
    
    Co-authored-by: Khomutov Dmitry <dmitriykhomutov@gmail.com>

--- kafka-connect/kafka-connect/src/test/java/org/apache/iceberg/connect/data/UnpartitionedDeltaWriterTest.java
+++ kafka-connect/kafka-connect/src/test/java/org/apache/iceberg/connect/data/UnpartitionedDeltaWriterTest.java
@@ -23,6 +23,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 import org.apache.iceberg.connect.IcebergSinkConfig;
 import org.apache.iceberg.connect.TableSinkConfig;
@@ -52,5 +53,27 @@ public class UnpartitionedDeltaWriterTest extends BaseWriterTest {
     // and 1 delete file
     assertThat(result.dataFiles()).hasSize(1);
     assertThat(result.deleteFiles()).hasSize(1);
+  }
+
+  @Test
+  public void testUnpartitionedDeltaWriterWithWrappedRows() {
+    IcebergSinkConfig config = mock(IcebergSinkConfig.class);
+    when(config.isUpsertMode()).thenReturn(true);
+    when(config.tableConfig(table.name()))
+        .thenReturn(new TableSinkConfig(Pattern.compile(""), Arrays.asList(), Arrays.asList(), ""));
+
+    List<Record> records =
+        ImmutableList.of(
+            wrappedRecord(123L, "part1", Operation.INSERT),
+            wrappedRecord(234L, "part1", Operation.UPDATE),
+            wrappedRecord(123L, "part1", Operation.DELETE),
+            wrappedRecord(456L, "part2", Operation.UPDATE));
+
+    WriteResult result = writeTest(records, config, UnpartitionedDeltaWriter.class);
+
+    // 1 append file because of 2 UPDATES and INSERT
+    // 2 delete files because of 2 UPDATES (equality delete) + 1 positional for DELETE
+    assertThat(result.dataFiles()).hasSize(1);
+    assertThat(result.deleteFiles()).hasSize(2);
   }
 }
