c85674486786dffe1294fb49dc1e35bed834bc92
Tigran Manasyan <t.manasyan@arenadata.io>
2026-04-09 21:32:26 +0100

    ADS-3252: Support reading empty Hadoop xml config

--- kafka-connect/kafka-connect/src/main/java/org/apache/iceberg/connect/CatalogUtils.java
+++ kafka-connect/kafka-connect/src/main/java/org/apache/iceberg/connect/CatalogUtils.java
@@ -24,6 +24,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 import org.apache.iceberg.CatalogUtil;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.common.DynClasses;
@@ -37,8 +39,14 @@ import org.slf4j.LoggerFactory;
 class CatalogUtils {
 
   private static final Logger LOG = LoggerFactory.getLogger(CatalogUtils.class.getName());
-  private static final List<String> HADOOP_CONF_FILES =
-      ImmutableList.of("core-site.xml", "hdfs-site.xml", "hive-site.xml");
+  static final List<String> HADOOP_CONF_FILES =
+      ImmutableList.of(
+          "core-site.xml",
+          "hdfs-default.xml",
+          "hdfs-site.xml",
+          "hive-site.xml",
+          "ozone-default.xml",
+          "ozone-site.xml");
 
   static Catalog loadCatalog(IcebergSinkConfig config) {
     return CatalogUtil.buildIcebergCatalog(
@@ -48,11 +56,7 @@ class CatalogUtils {
   // use reflection here to avoid requiring Hadoop as a dependency
   private static Object loadHadoopConfig(IcebergSinkConfig config) {
     Class<?> configClass =
-        DynClasses.builder()
-            .impl("org.apache.hadoop.hdfs.HdfsConfiguration")
-            .impl("org.apache.hadoop.conf.Configuration")
-            .orNull()
-            .build();
+        DynClasses.builder().impl("org.apache.hadoop.conf.Configuration").orNull().build();
 
     if (configClass == null) {
       LOG.info("Hadoop not found on classpath, not creating Hadoop config");
@@ -68,19 +72,14 @@ class CatalogUtils {
 
       //  load any config files in the specified config directory
       String hadoopConfDir = config.hadoopConfDir();
-      if (hadoopConfDir != null) {
-        HADOOP_CONF_FILES.forEach(
-            confFile -> {
-              Path path = Paths.get(hadoopConfDir, confFile);
-              if (Files.exists(path)) {
-                try {
-                  addResourceMethod.invoke(path.toUri().toURL());
-                } catch (IOException e) {
-                  LOG.warn("Error adding Hadoop resource {}, resource was not added", path, e);
-                }
-              }
-            });
-      }
+
+      Function<String, Path> resourcePathResolver =
+          hadoopConfDir != null
+              ? resource -> Paths.get(hadoopConfDir, resource)
+              : CatalogUtils::getClasspathResource;
+
+      HADOOP_CONF_FILES.forEach(
+          resource -> loadResource(resource, resourcePathResolver, addResourceMethod));
 
       // set any Hadoop properties specified in the sink config
       config.hadoopProps().forEach(setMethod::invoke);
@@ -92,6 +91,26 @@ class CatalogUtils {
           "Hadoop found on classpath but could not create config, proceeding without config", e);
     }
     return null;
+  }
+
+  private static void loadResource(
+      String resource, Function<String, Path> resourcePathResolver, BoundMethod addResourceMethod) {
+    Path path = resourcePathResolver.apply(resource);
+    try {
+      if (path != null && Files.exists(path) && Files.size(path) > 0) {
+        addResourceMethod.invoke(path.toUri().toURL());
+      }
+    } catch (IOException e) {
+      LOG.warn("Error adding Hadoop resource {}, resource was not added", path, e);
+    }
+  }
+
+  private static Path getClasspathResource(String resourcePath) {
+    URL resource =
+        Optional.ofNullable(Thread.currentThread().getContextClassLoader())
+            .orElseGet(CatalogUtils.class::getClassLoader)
+            .getResource(resourcePath);
+    return Optional.ofNullable(resource).map(URL::getPath).map(Paths::get).orElse(null);
   }
 
   private CatalogUtils() {}
