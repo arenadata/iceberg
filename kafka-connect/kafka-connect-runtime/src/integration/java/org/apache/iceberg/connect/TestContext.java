ad88aba499a6412d7f57cc39a9f302fe76b0e4d9
mantasheva-dzh <mds@arenadata.io>
2026-06-22 19:32:26 +0300

    removing redundant config

--- kafka-connect/kafka-connect-runtime/src/integration/java/org/apache/iceberg/connect/TestContext.java
+++ kafka-connect/kafka-connect-runtime/src/integration/java/org/apache/iceberg/connect/TestContext.java
@@ -60,7 +60,6 @@ public class TestContext {
   private TestContext() {
     ComposeContainer container =
         new ComposeContainer(new File("./docker/docker-compose.yml"))
-            .withEnv("ICEBERG_VERSION", System.getProperty("project.version"))
             .withStartupTimeout(Duration.ofMinutes(2))
             .waitingFor("connect", Wait.forHttp("/connectors"));
     container.start();
